5be587e66de9e75651d1c553cba5c98646d00a8d
Aabiskar <Copywriterayan47@gmail.com>
2023-11-23 15:28:33 +0545

    frontend garda garda jhau layo aba backend ko crud garxu hai

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -5,40 +5,41 @@ import { IoIosPersonAdd, IoMdSettings } from "react-icons/io";
 import { FaAddressBook } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 import { RiLogoutCircleRLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebarContainer}>
-      <div className={styles.iconWrapper}>
+      <Link to={"/"} className={styles.iconWrapper}>
         <AiFillHome className={styles.icons} />
 
         <p className={styles.sidebarPages}>Home</p>
-      </div>
-      <div className={styles.iconWrapper}>
+      </Link>
+      <Link className={styles.iconWrapper}>
         <IoIosPersonAdd className={styles.icons} />
 
         <p className={styles.sidebarPages}>Add Contact</p>
-      </div>
-      <div className={styles.iconWrapper}>
+      </Link>
+      <Link className={styles.iconWrapper}>
         <FaAddressBook className={styles.icons} />
 
         <p className={styles.sidebarPages}>View Contact</p>
-      </div>
-      <div className={styles.iconWrapper}>
+      </Link>
+      <Link className={styles.iconWrapper}>
         <CgProfile className={styles.icons} />
 
         <p className={styles.sidebarPages}>Your Profile</p>
-      </div>
-      <div className={styles.iconWrapper}>
+      </Link>
+      <Link className={styles.iconWrapper}>
         <IoMdSettings className={styles.icons} />
 
         <p className={styles.sidebarPages}>Setting</p>
-      </div>
-      <div className={styles.iconWrapper}>
+      </Link>
+      <Link className={styles.iconWrapper}>
         <RiLogoutCircleRLine className={styles.icons} />
 
         <p className={styles.sidebarPages}>Logout</p>
-      </div>
+      </Link>
     </div>
   );
 };
