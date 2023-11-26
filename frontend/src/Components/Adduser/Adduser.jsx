efb0ca42571d1484af310d18644b5f57ce9c1c5f
Aabiskar <Copywriterayan47@gmail.com>
2023-11-26 16:31:37 +0545

    completed the design of form  that adds user but not worked at funcionality yet

--- frontend/src/Components/Adduser/Adduser.jsx
+++ frontend/src/Components/Adduser/Adduser.jsx
@@ -1,17 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Adduser.module.css";
+import { IoIosPersonAdd } from "react-icons/io";
+import { IoMail } from "react-icons/io5";
+import { LuPhone } from "react-icons/lu";
+import { RiProfileFill } from "react-icons/ri";
+import { MdDescription } from "react-icons/md";
+import { MdImage } from "react-icons/md";
 
 const Adduser = () => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [image, setImage] = useState();
+  const [phone, setPhone] = useState();
+  const [profession, setProfession] = useState();
+  const [description, setDescription] = useState();
+  const [owner, setOwner] = useState();
+
   return (
     <div className={styles.adduserContainer}>
       <div className={styles.container}>
+        <h1>Add Contact</h1>
+        {/* username wrapper */}
         <div className={styles.inputWrapper}>
-          {/* icon */}
+          <div className={styles.iconWrapper}>
+            <IoIosPersonAdd className={styles.icons} />
+          </div>
           <input
             className={styles.inputBox}
             placeholder="Enter the name"
             type="text"
           />
+        </div>
+        {/* emailWrapper */}
+        <div className={styles.inputWrapper}>
+          <div className={styles.iconWrapper}>
+            <IoMail className={styles.icons} />
+          </div>
+          <input
+            className={styles.inputBox}
+            placeholder="Enter the email"
+            type="text"
+          />
+        </div>
+        {/* imageWrapper */}
+        <div className={styles.inputWrapper}>
+          <div className={styles.iconWrapper}>
+            <MdImage className={styles.icons} />
+          </div>
+          <input
+            className={styles.inputBox}
+            placeholder="Enter the image"
+            type="text"
+          />
+        </div>
+        {/* phone Wrapper */}
+        <div className={styles.inputWrapper}>
+          <div className={styles.iconWrapper}>
+            <LuPhone className={styles.icons} />
+          </div>
+          <input
+            className={styles.inputBox}
+            placeholder="Enter the phone number"
+            type="number"
+          />
+        </div>
+        {/* profeesion wrapper */}
+        <div className={styles.inputWrapper}>
+          <div className={styles.iconWrapper}>
+            <RiProfileFill className={styles.icons} />
+          </div>
+          <input
+            className={styles.inputBox}
+            placeholder="Enter the Profession"
+            type="text"
+          />
+        </div>
+        {/* description wrapper */}
+        <div className={styles.inputWrapper}>
+          <div className={styles.iconWrapper}>
+            <MdDescription className={styles.icons} />
+          </div>
+          <input
+            className={styles.inputBox}
+            placeholder="Enter the Description"
+            type="text"
+          />
+        </div>
+        <div className={styles.buttonWrapper}>
+          <button className={styles.AddBtn}> Add </button>
         </div>
       </div>
     </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,8 @@ import Viewcontact from "./Pages/ViewContact/Viewcontact.jsx";
 import Setting from "./Pages/Setting/Setting.jsx";
 import Profile from "./Pages/Profile/Profile.jsx";
 import DashBoard from "./Pages/Dashboard/Dashboard.jsx";
-import Nouser from "./Components/Nouser/Nouser.jsx";
+// import Nouser from "./Components/Nouser/Nouser.jsx";
+import Adduser from "./Components/Adduser/Adduser.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -18,7 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<DashBoard />}>
-        <Route path="addcontact" element={<Nouser />} />
+        <Route path="addcontact" element={<Adduser />} />
         <Route path="setting" element={<Setting />} />
         <Route path="viewcontact" element={<Viewcontact />} />
         <Route path="profile" element={<Profile />} />
